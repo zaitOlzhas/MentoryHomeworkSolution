8ee1eaa30a5ee5944cd155c81c819ae4b39d27b2
Olzhas Zait <zait.olzhas@gmail.com>
2023-12-11 20:45:42 +0600

    Added Comments.

--- zait_olzhas/ArrayTasksOfMine.cs
+++ zait_olzhas/ArrayTasksOfMine.cs
@@ -4,6 +4,11 @@ namespace zait_olzhas
 {
     public class ArrayTasksOfMine : IArrayTasks
     {
+        /// <summary>
+        /// This method returns sum of all numbers in array.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
         public decimal GetSumOfAllNumbers(decimal[] numbers)
         {
             decimal sum = 0;
