c73bcb97887a8793929fde1a4c5df5b6cd44ed03
usa-olia <issakov.alexey80@gmail.com>
2023-12-13 08:41:50 +0600

    Написание класса для проверки

--- issakov_alexey/Array.cs
+++ issakov_alexey/Array.cs
@@ -1,7 +1,18 @@
-﻿namespace issakov_alexey
-{
-    public class Array
-    {
+﻿using MainHomeworkRequirements;
 
+namespace issakov_alexey
+{
+
+    public class ArrayTasksOfMine : IArrayTasks
+    {
+        public decimal GetSumOfAllNumbers(decimal[] numbers)
+        {
+            decimal sum = 0;
+            foreach (decimal number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
     }
-}
+
