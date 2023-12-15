f5ccf66bacbd4b44dee5135b0f17f644ed8a6bb9
askhatzhax <ash.zhaxybaev001@gmail.com>
2023-12-15 22:29:47 +0600

    Добавил проект для дз(zhaxybayev_askhat) и выполнил первое задания и проверил его юнит тесте

---
+++ zhaxybayev_askhat/ArrayTasksSolution.cs
@@ -0,0 +1,16 @@
+﻿using MainHomeworkRequirements;
+
+namespace zhaxybayev_askhat
+{
+    public class ArrayTasksSolution : IArrayTasks
+    {
+        public decimal GetSumOfAllNumbers(decimal[] numbers)
+        {
+            decimal summary = 0;
+            foreach (var number in numbers) {
+                summary += number;
+            }
+            return summary;
+        }
+    }
+}
