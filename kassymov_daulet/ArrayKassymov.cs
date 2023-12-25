74601d3c04d4586c642e2319673e8cd2764a4f54
dauletkassymov <153493243+dauletkassymov@users.noreply.github.com>
2023-12-25 19:13:45 +0600

    Kassymov Daulet (#9)
    Co-authored-by: Olzhas <zait.olzhas@gmail.com>

---
+++ kassymov_daulet/ArrayKassymov.cs
@@ -0,0 +1,17 @@
+﻿using MainHomeworkRequirements;
+
+namespace kassymov_daulet
+{
+    public class ArrayKassymov : IArrayTasks
+    {
+        public decimal GetSumOfAllNumbers(decimal[] numbers)
+        {
+            decimal sum = 0;
+            foreach (decimal number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
