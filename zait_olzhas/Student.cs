e0989372332d143504c42afa47fc2f40ec5bab79
Olzhas Zait <zait.olzhas@gmail.com>
2024-01-15 11:40:34 +0600

    IComparable task changes.

---
+++ zait_olzhas/Student.cs
@@ -0,0 +1,22 @@
+﻿using MainHomeworkRequirements.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zait_olzhas
+{
+    public class Student : Person
+    {
+        public int StudentId { get; set; }
+        public Student(string name, int age, int id) : base(name, age)
+        {
+            StudentId = id;
+        }
+        public override string ToString()
+        {
+            return $"StudentId: {StudentId}, Name: {Name}, Age: {Age}";
+        }
+    }
+}
