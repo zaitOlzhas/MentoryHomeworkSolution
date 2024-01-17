227eb2630e9b1ad9230a6b7ba73c712c00914583
askhatzhax <74249373+askhatzhax@users.noreply.github.com>
2024-01-17 08:39:49 +0600

    Askhat zhaxybayev (#20)
    IComparer and IComparable homework

---
+++ zhaxybayev_askhat/StudentComparer.cs
@@ -0,0 +1,20 @@
+﻿using MainHomeworkRequirements.Classes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zhaxybayev_askhat
+{
+    public class IdStudentComparer : IComparer<Student>
+    {
+        public int Compare(Student? s1, Student? s2)
+        {
+            if (s1 is null || s2 is null)
+                throw new ArgumentException("Некорректное значение параметра");
+            return s1.Id - s2.Id;
+        }
+    }
+}
