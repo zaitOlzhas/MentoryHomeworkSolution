a8015c9c77a9035f81897f570cd91e054a02dde1
usa-olia <issakov.alexey80@gmail.com>
2024-01-18 12:08:39 +0600

    Создал класс StudentIdComparer

---
+++ issakov_alexey/StudentComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace issakov_alexey
+{
+    public class StudentIdComparer : IComparer<Student>
+    {
+        public int Compare(Student? x, Student? y)
+        {
+            if (x is null || y is null) throw new ArgumentException("Input value cannot be null.");
+            return x.StudentId.CompareTo(y.StudentId);
+        }
+    }
+}
