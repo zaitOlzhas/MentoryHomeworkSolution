83be5e0d963649ed582608394c628a00bbf59dd5
DaniyarYermagambetov <65025160+sqwa11@users.noreply.github.com>
2024-01-18 22:27:06 +0600

    My second commit (#23)

---
+++ Yermagambetov_Daniyar/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yermagambetov_Daniyar
+{
+    public class Student : IComparable<Student>
+    {
+        public string Name { get; }
+        public int Age { get; set; }
+        public int StudentId { get; set; }
+
+        public Student(string name, int age, int id)
+        {
+            StudentId = id; Name = name; Age = age;
+        }
+        public int CompareTo(Student? person)
+        {
+            if (person is null) throw new ArgumentException("Некорректное значение параметра");
+            return StudentId.CompareTo(person.StudentId);
+        }
+        public override string ToString()
+        {
+            return $"StudentId: {StudentId}, Name: {Name}, Age: {Age}";
+        }
+    }
+}
