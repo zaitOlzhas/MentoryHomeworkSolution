5bc156b2a6d0335a9297f43fe4d655f246a2eb8e
DaniyarYermagambetov <65025160+sqwa11@users.noreply.github.com>
2024-02-05 22:30:58 +0600

    Наконец то реализовал Person (#34)

--- Yermagambetov_Daniyar/Student.cs
+++ Yermagambetov_Daniyar/Student.cs
@@ -1,29 +1,26 @@
-﻿using System;
+﻿using MainHomeworkRequirements.Classes;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Yermagambetov_Daniyar
 {
-    public class Student : IComparable<Student>
+    public class Student : Person,  IComparable<Student>
     {
-        public string Name { get; }
-        public int Age { get; set; }
         public int StudentId { get; set; }
 
-        public Student(string name, int age, int id)
+        public Student(string name, int age, int id) : base(name, age)
         {
-            StudentId = id; Name = name; Age = age;
+            StudentId = id; 
         }
-        public int CompareTo(Student? person)
+
+        public int CompareTo(Student? other)
         {
-            if (person is null) throw new ArgumentException("Некорректное значение параметра");
-            return StudentId.CompareTo(person.StudentId);
-        }
-        public override string ToString()
-        {
-            return $"StudentId: {StudentId}, Name: {Name}, Age: {Age}";
+            if (other is null) throw new ArgumentNullException("Не присвоено значение.");
+            else return StudentId.CompareTo(other.StudentId);
         }
     }
 }
