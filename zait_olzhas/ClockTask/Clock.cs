baa90b97b167d536b7e50678048276f78630e801
Olzhas Zait <zait.olzhas@gmail.com>
2024-02-05 20:32:54 +0600

    Iclock homework

---
+++ zait_olzhas/ClockTask/Clock.cs
@@ -0,0 +1,98 @@
+﻿using MainHomeworkRequirements;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace zait_olzhas.ClockTask
+{
+    public class Clock : IClock
+    {
+        private bool isRunning = false;
+        private Timer timer;
+        private DateTime alarmTime;
+        public DateTime AlarmTime
+        {
+            get
+            {
+                return alarmTime;
+            }
+            set
+            {
+                if (alarmTime != value)
+                {
+                    alarmTime = value;
+                    AlarmSet?.Invoke(this);
+                }
+            }
+        }
+        public TimeSpan SnoozeTime { get; set; } = TimeSpan.FromMinutes(1); 
+
+        public DateTime ClockTime { get; private set; }
+
+        public event TimeHandler? ClockStarted;
+        public event TimeHandler? ClockStopped;
+        public event TimeHandler? SecondChanged;
+        public event TimeHandler? MinuteChanged;
+        public event TimeHandler? HourChanged;
+        public event TimeHandler? Alarm;
+        public event TimeHandler? AlarmSet;
+        public event TimeHandler? SnoozeActivated;
+        public event TimeHandler? FalseStop;
+        public event TimeHandler? FalseStart;
+
+        public Clock()
+        {
+            timer = new Timer(1000); 
+            timer.Elapsed += TimerElapsed;
+        }
+        public void Start()
+        {
+            if (isRunning)
+            {
+                FalseStart?.Invoke(this);
+                return;
+            }
+
+            timer.Start();
+            isRunning = true;
+            ClockStarted?.Invoke(this);
+        }
+        public void Stop()
+        {
+            if (!isRunning)
+            {
+                FalseStop?.Invoke(this);
+                return;
+            }
+
+            timer.Stop();
+            isRunning = false;
+            ClockStopped?.Invoke(this);
+        }
+        public void Snooze()
+        {
+            AlarmTime = DateTime.Now.Add(SnoozeTime);
+            SnoozeActivated?.Invoke(this);
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            ClockTime = DateTime.Now;
+            SecondChanged?.Invoke(this);
+
+            if (ClockTime.Second == 0)
+            {
+                MinuteChanged?.Invoke(this);
+            }
+
+            if (ClockTime.Minute == 0 && ClockTime.Second == 0)
+            {
+                HourChanged?.Invoke(this);
+            }
+
+            if (ClockTime.Hour == AlarmTime.Hour && ClockTime.Minute == AlarmTime.Minute && ClockTime.Second == AlarmTime.Second)
+            {
+                Alarm?.Invoke(this);
+            }
+        }
+    }
+}
+
---
+++ zait_olzhas/ClockTask/Clock1.cs
@@ -0,0 +1,42 @@
+﻿using MainHomeworkRequirements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zait_olzhas.ClockTask
+{
+    internal class Clock1 : IClock
+    {
+        public DateTime ClockTime => throw new NotImplementedException();
+
+        public DateTime AlarmTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event TimeHandler ClockStarted;
+        public event TimeHandler ClockStopped;
+        public event TimeHandler Alarm;
+        public event TimeHandler AlarmSet;
+        public event TimeHandler HourChanged;
+        public event TimeHandler MinuteChanged;
+        public event TimeHandler SecondChanged;
+        public event TimeHandler SnoozeActivated;
+        public event TimeHandler FalseStop;
+        public event TimeHandler FalseStart;
+
+        public void Snooze()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
