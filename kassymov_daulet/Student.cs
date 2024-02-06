e3316bc829ed627009bf841c4afa0b11d489ef34
dauletkassymov <daulet.20@mail.ru>
2024-02-06 15:31:34 +0600

    Daulet Kassymov IComparable and IComparer

---
+++ kassymov_daulet/Student.cs
@@ -0,0 +1,29 @@
+﻿using MainHomeworkRequirements.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace kassymov_daulet
+{
+    public class Student : Person, IComparable<Student>
+    {
+        public int StudentId { get; set; }
+        public Student(string name, int age, int id) : base(name, age)
+        {
+            StudentId = id;
+        }
+        public override string ToString()
+        {
+            return $"StudentId: {StudentId}, Name: {Name}, Age: {Age}";
+        }
+
+        public int CompareTo(Student? other)
+        {
+            if (other is null) throw new ArgumentNullException("Input value cannot be null.");
+            else return StudentId.CompareTo(other.StudentId);
+        }
+    }
+}
