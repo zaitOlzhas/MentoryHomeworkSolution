ff9b4e5881e0ce5a7141b024be0d6ab56ab02241
dauletkassymov <153493243+dauletkassymov@users.noreply.github.com>
2024-02-08 20:09:09 +0600

    Daulet Kassymov IComparable and IComparer (#36)

---
+++ kassymov_daulet/StudentComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kassymov_daulet
+{
+    public class StudentComparer : IComparer<Student>
+    {
+        public int Compare(Student? x, Student? y)
+        {
+            if (x is null || y is null) throw new ArgumentException("Input value cannot be null.");
+
+            return x.StudentId.CompareTo(y.StudentId);
+
+        }
+    }
+}
