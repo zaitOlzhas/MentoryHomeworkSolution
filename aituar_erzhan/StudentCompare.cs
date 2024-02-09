3d8889975ed0688fe0f0040ab9b483d66f4e6504
Ержан Айтуар <aerzhan9@gmail.com>
2024-02-09 15:46:34 +0600

    StudentCompare

---
+++ aituar_erzhan/StudentCompare.cs
@@ -0,0 +1,13 @@
+﻿namespace aituar_erzhan
+{
+    public class StudentComparer : IComparer<Student>
+    {
+        public int Compare(Student? x, Student? y)
+        {
+            if (x is null || y is null) throw new ArgumentException("Некорректное значение параметра");
+
+            return x.StudentId.CompareTo(y.StudentId);
+
+        }
+    }
+}
