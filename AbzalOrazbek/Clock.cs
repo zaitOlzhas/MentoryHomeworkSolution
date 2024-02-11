f578a6516d8d3882dac323785230767c0c73f75f
Abzal Orazbek <abzalorazbek2003@gmail.com>
2024-02-12 05:46:53 +0600

    Implement Clock class that implements IClock

---
+++ AbzalOrazbek/Clock.cs
@@ -0,0 +1,91 @@
+﻿using MainHomeworkRequirements;
+
+namespace AbzalOrazbek
+{
+    internal class Clock : IClock
+    {
+        private bool isRunning = false;
+        public DateTime ClockTime => _currentTime;
+        public DateTime AlarmTime
+        {
+            get => _alarmTime;
+            set
+            {
+                _alarmTime = value;
+                AlarmSet?.Invoke(this);
+            }
+        }
+        private DateTime _alarmTime { get; set; }
+        private DateTime _currentTime { get; set; }
+
+        public event TimeHandler? ClockStarted;
+        public event TimeHandler? ClockStopped;
+        public event TimeHandler? Alarm;
+        public event TimeHandler? AlarmSet;
+        public event TimeHandler? HourChanged;
+        public event TimeHandler? MinuteChanged;
+        public event TimeHandler? SecondChanged;
+        public event TimeHandler? SnoozeActivated;
+        public event TimeHandler? FalseStop;
+        public event TimeHandler? FalseStart;
+
+        public void Snooze()
+        {
+            AlarmTime = DateTime.Now.AddSeconds(5);
+            SnoozeActivated?.Invoke(this);
+        }
+
+        public void Start()
+        {
+            if (isRunning)
+            {
+                FalseStart?.Invoke(this);
+                AlarmSet?.Invoke(this);
+            }
+            else
+            {
+                AlarmSet?.Invoke(this);
+                ClockStarted?.Invoke(this);
+                isRunning = true;
+                _ = RunClock();
+            }
+        }
+
+        public void Stop()
+        {
+            if (!isRunning)
+            {
+                FalseStop?.Invoke(this);
+            }
+            else
+            {
+                isRunning = false;
+                ClockStopped?.Invoke(this);
+            }
+        }
+        private async Task RunClock()
+        {
+            while (isRunning)
+            {
+                await Task.Delay(1000);
+                if (_currentTime.Second != DateTime.Now.Second)
+                {
+                    SecondChanged?.Invoke(this);
+                }
+                if (_currentTime.Minute != DateTime.Now.Minute)
+                {
+                    MinuteChanged?.Invoke(this);
+                }
+                if (_currentTime.Hour != DateTime.Now.Hour)
+                {
+                    HourChanged?.Invoke(this);
+                }
+                if (AlarmTime == DateTime.Now)
+                {
+                    Alarm?.Invoke(this);
+                }
+                _currentTime = DateTime.Now;
+            }
+        }
+    }
+}
--- AbzalOrazbek/Program.cs
+++ AbzalOrazbek/Program.cs
@@ -17,6 +17,23 @@
             students.Sort();
             students.ForEach(student => Console.Write(student.Name + " "));
             Console.WriteLine();
+
+            Clock clock = new Clock();
+            clock.ClockStarted += (sender) => Console.WriteLine("Clock started");
+            clock.ClockStopped += (sender) => Console.WriteLine("Clock stopped");
+            clock.Alarm += (sender) => Console.WriteLine("Alarm");
+            clock.AlarmSet += (sender) => Console.WriteLine("Alarm set");
+            clock.HourChanged += (sender) => Console.WriteLine("Hour changed");
+            clock.MinuteChanged += (sender) => Console.WriteLine("Minute changed");
+            clock.SecondChanged += (sender) => Console.WriteLine("Second changed");
+            clock.SnoozeActivated += (sender) => Console.WriteLine("Snooze activated");
+            clock.FalseStop += (sender) => Console.WriteLine("False stop");
+            clock.FalseStart += (sender) => Console.WriteLine("False start");
+            clock.Start();
+            clock.AlarmTime = DateTime.Now.AddSeconds(5);
+            clock.Snooze();
+            Thread.Sleep(60000); // Sleep for 1 minute while clock is running
+            clock.Stop();
         }
     }
 }
