08f4283933a563e9cbec5fd1645dab7bda0b2fba
Abzal Orazbek <abzalorazbek2003@gmail.com>
2024-02-12 04:50:08 +0600

    Implement Student class with IComparable and IComparer

---
+++ AbzalOrazbek/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AbzalOrazbek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+                new(1, "Abzal", 20),
+                new(5, "Orazbek", 21),
+                new(4, "Another Student 1", 19),
+                new(2, "Test student", 22),
+                new(3, "Student)", 23)
+            };
+            students.ForEach(student => Console.Write(student.Name + " "));
+            Console.WriteLine();
+            students.Sort();
+            students.ForEach(student => Console.Write(student.Name + " "));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AbzalOrazbek/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace AbzalOrazbek
+{
+    public class Student : IComparable<Student>, IComparer<Student>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public Student() { }
+        public Student(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public int CompareTo(Student? other)
+        {
+            return Id.CompareTo(other?.Id);
+        }
+
+        public int Compare(Student? x, Student? y)
+        {
+            return x!.Id.CompareTo(y?.Id);
+        }
+    }
+}
