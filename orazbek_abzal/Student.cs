11e5661113cfaf2265bf1512865a790fca3361a6
Abzal Orazbek <abzalorazbek2003@gmail.com>
2024-02-12 22:43:37 +0600

    Implement IComparable<T> in Student class

---
+++ orazbek_abzal/Student.cs
@@ -0,0 +1,27 @@
+﻿using MainHomeworkRequirements.Classes;
+
+namespace orazbek_abzal
+{
+    public class Student : Person, IComparable<Student>
+    {
+        private static int _id = 0;
+        public int Id { get; set; }
+
+        public Student(string name, int age) : base(name, age)
+        {
+            Id = _id++;
+        }
+        public Student(string name, int age, int id) : base(name, age)
+        {
+            Id = id;
+        }
+        public int CompareTo(Student? other)
+        {
+            return Id.CompareTo(other?.Id);
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Age: {Age}";
+        }
+    }
+}
