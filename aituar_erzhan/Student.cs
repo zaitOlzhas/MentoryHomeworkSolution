75182672984119c5c12ac1a5296bf03b0e7a1e71
aerzhan <116055682+aerzhan@users.noreply.github.com>
2024-02-12 09:39:36 +0600

    StudentCompare (#37)

---
+++ aituar_erzhan/Student.cs
@@ -0,0 +1,23 @@
+﻿using MainHomeworkRequirements.Classes;
+
+namespace aituar_erzhan
+{
+    public class Student : Person, IComparable<Student>
+    {
+        public int StudentId { get; set; }
+        public Student(string name, int age, int id) : base(name, age)
+        {
+            StudentId = id;
+        }
+        public override string ToString()
+        {
+            return $"StudentId: {StudentId}, Name: {Name}, Age: {Age}";
+        }
+
+        public int CompareTo(Student? other)
+        {
+            if (other is null) throw new ArgumentNullException("Некорректное значение параметра");
+            else return StudentId.CompareTo(other.StudentId);
+        }
+    }
+}
