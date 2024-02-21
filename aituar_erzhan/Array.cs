ca524fc23c77c879010b62e635894e0cec7c6078
aerzhan <116055682+aerzhan@users.noreply.github.com>
2024-02-21 18:17:17 +0600

    Iclock (#44)

---
+++ aituar_erzhan/Array.cs
@@ -0,0 +1,18 @@
+﻿using MainHomeworkRequirements;
+
+namespace aituar_erzhan
+{
+    public class Array : IArrayTasks
+    {
+        public decimal GetSumOfAllNumbers(decimal[] numbers)
+        {
+            decimal sum = 0;
+            foreach (decimal number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
+
